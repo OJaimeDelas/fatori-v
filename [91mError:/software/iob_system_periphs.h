510b281990af57dc7454e66e9ec350be101b3477
OJaimeDelas <minde.jaime@gmail.com>
2025-02-25 16:54:15 +0000

    Last Update on Old_Py2. Error with AXI slave selection

---
+++ [91mError:/software/iob_system_periphs.h
@@ -0,0 +1,4 @@
+#define UART0_BASE (PBUS_BASE + (0 << (P_BIT - 2)))
+#define TIMER0_BASE (PBUS_BASE + (1 << (P_BIT - 2)))
+#define CLINT0_BASE (PBUS_BASE + (2 << (P_BIT - 2)))
+#define PLIC0_BASE (PBUS_BASE + (3 << (P_BIT - 2)))
